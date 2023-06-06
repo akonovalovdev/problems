d7283f582821a1617f16f22ef2e20a8f38cb8acc
Александр <akonovalov.dev@gmail.com>
2023-06-06 21:31:25 +0300

    Adds comments for two sum

--- two-sum/two_sum.go
+++ two-sum/two_sum.go
@@ -1,13 +1,16 @@
 package main
 
 func twoSum(nums []int, target int) []int {
+	// создайм мапу, где ключами будут разница искомой суммы и значения проверяемых индексов
 	memory := make(map[int]int)
 	for i := 0; i < len(nums); i++ {
-		raznitsa := target - nums[i]
-		index, ok := memory[raznitsa]
+		raznitsa := target - nums[i]  // записываем разницу
+		index, ok := memory[raznitsa] // проверяем существование ключа - разницы
 		if ok {
+			//если нашли то возвращаем текущий индекс и значение(индекс) того ключа сто нашли
 			return []int{index, i}
 		}
+		// если не нашли то в значение мапы записываем текущий индекс
 		memory[nums[i]] = i
 	}
 	return nil
