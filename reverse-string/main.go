66685143bdee89565947be6201693b4248f2c929
Александр <akonovalov.dev@gmail.com>
2023-06-07 21:46:01 +0300

    Adds new directory reverse string with task condition

---
+++ reverse-string/main.go
@@ -0,0 +1,13 @@
+package main
+
+// Дана строка input. Нужно написать функцию, которая перевернет строку справа налево.
+
+import "fmt"
+
+func main() {
+
+	fmt.Println(reverseString("Привет я Петя!"))                  // !ятеП я тевирП
+	fmt.Println(reverseString("Мой номер телефона: 89167854141")) // 14145879168 :анофелет ремон йоМ
+	fmt.Println(reverseString("Hello, 世界"))                     // 界世 , olleH
+
+}
