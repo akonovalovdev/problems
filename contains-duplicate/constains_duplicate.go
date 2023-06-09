4d9f17b7a54a00b6d13a7851c574ed8f2fd17d46
Александр <akonovalov.dev@gmail.com>
2023-06-09 11:23:28 +0300

    Fixed constains duplicate

--- contains-duplicate/constains_duplicate.go
+++ contains-duplicate/constains_duplicate.go
@@ -2,14 +2,15 @@ package main
 
 func constainsDuplicate(nums []int) bool {
 	//создаём мапу, куда будем записывать и сравнивать уникальные элементы массива
-	m := map[int]int{}
+	m := make(map[int]int, len(nums))
 	for i := 0; i < len(nums); i++ {
 		_, ok := m[nums[i]] // проверяем есть ли в мапе уже такой ключ
 		if !ok {
 			m[nums[i]] = 1 // если ключ не найден, то записываем его
-		} else {
-			return true // если найден, то возвращаем правду
+			continue
 		}
+		return true // если найден, то возвращаем правду
+
 	}
-	return false // если не найден то возвращаем ложь
+	return false // если не найден за весь цикл, то возвращаем ложь
 }
