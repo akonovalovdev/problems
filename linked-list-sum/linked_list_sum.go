804be3704c5105988b9fac1d4f9d030d8c504c96
Александр <akonovalov.dev@gmail.com>
2023-06-11 20:29:11 +0300

    Adds solution to the problem linked list sum

---
+++ linked-list-sum/linked_list_sum.go
@@ -0,0 +1,55 @@
+package main
+
+import "fmt"
+
+type ListNode struct {
+	Value int       // пустое значение - 0
+	Next  *ListNode // пустое значение - nil
+}
+
+func (node *ListNode) ToSlice() (digits []int) {
+	for current := node; current != nil; current = current.Next {
+		digits = append(digits, current.Value)
+	}
+	return digits
+}
+
+func NewListNode(digits []int) *ListNode {
+	// это нужно потому(давайте предствим что этого нет) если мы вернём node,
+	// и мы не сможем обратиться к другим элементам этого связного списка, так как это конец
+	// то мы вернём последний элемент связного списка, а нам нужно вернуть
+	// голову - указатель на начало связного списка
+	head := &ListNode{}
+	node := head
+	for i := 0; i < len(digits); i++ {
+		node.Next = &ListNode{
+			Value: digits[i],
+			Next:  nil,
+		}
+		fmt.Printf("%+v\n", node)
+		node = node.Next
+	}
+	return head.Next
+}
+
+func sumLinkedLists(a *ListNode, b *ListNode) *ListNode { //функция возвращает указательна на созданную структуру
+	carry, dummy := 0, new(ListNode) //&ListNode{} мы создаём указатель(поинтер) на пустую структуру
+	node := dummy
+	for a != nil || b != nil || carry > 0 {
+		if a != nil {
+			carry += a.Value
+			a = a.Next
+		}
+		if b != nil {
+			carry += b.Value
+			b = b.Next
+		}
+		node.Next = &ListNode{
+			Value: carry % 10,
+			Next:  nil,
+		}
+		node = node.Next
+		carry /= 10
+	}
+	return dummy.Next
+}
