84aa762cefc826b86a7d3a13a07e1354a1e4584f
Александр <akonovalov.dev@gmail.com>
2023-06-15 13:43:32 +0300

    Adds solution to the problem tree/same binary tree

--- tree_cases/binary/same_tree/same_tree.go
+++ tree_cases/binary/same_tree/same_tree.go
@@ -1,42 +1,6 @@
 package main
 
-import (
-	"fmt"
-)
-
-// Имея корни двух бинарных деревьев p и q, напишите функцию, проверяющую, совпадают ли они или нет.
-//
-// Два бинарных дерева считаются одинаковыми, если они структурно идентичны, а узлы имеют одинаковое значение.
-
-// Объявление узла двоичного дерева
-type TreeNode struct {
-	Val   int
-	Left  *TreeNode
-	Right *TreeNode
-}
-
-func main() {
-	//Вход: p = [1,2,3], q = [1,2,3]
-	p := &TreeNode{
-		Val: 1,
-		Left: &TreeNode{
-			Val: 2,
-		},
-		Right: &TreeNode{
-			Val: 3,
-		},
-	}
-	q := &TreeNode{
-		Val: 1,
-		Left: &TreeNode{
-			Val: 2,
-		},
-		Right: &TreeNode{
-			Val: 3,
-		},
-	}
-	fmt.Println(isSameTree(p, q))
-}
+import "fmt"
 
 func isSameTree(p *TreeNode, q *TreeNode) bool {
 	if p == nil {
@@ -79,3 +43,29 @@ func isSameTree(p *TreeNode, q *TreeNode) bool {
 
 	return true
 }
+
+//
+//func (n *TreeNode) Insert(value int) error {
+//
+//	if n == nil {
+//		return errors.New("Cannot insert a value into a nil tree")
+//	}
+//
+//	switch {
+//	case value == n.Val:
+//		return nil
+//	case value < n.Val:
+//		if n.Left == nil {
+//			n.Left = &TreeNode{Val: value}
+//			return nil
+//		}
+//		return n.Left.Insert(value)
+//	case value > n.Val:
+//		if n.Right == nil {
+//			n.Right = &TreeNode{Val: value}
+//			return nil
+//		}
+//		return n.Right.Insert(value)
+//	}
+//	return nil
+//}
