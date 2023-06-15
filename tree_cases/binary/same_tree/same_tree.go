5a4cd3bc30a1b4e2af1ab0e83479509409d40e34
Александр <akonovalov.dev@gmail.com>
2023-06-15 14:05:53 +0300

    Adds comments for tree/ same binary tree

--- tree_cases/binary/same_tree/same_tree.go
+++ tree_cases/binary/same_tree/same_tree.go
@@ -3,29 +3,31 @@ package main
 import "fmt"
 
 func isSameTree(p *TreeNode, q *TreeNode) bool {
-	if p == nil {
-		return q == nil
+	if p == nil { // исключаем панику в случаях, когда одно или несколько деревьев на входе пришли со значением nil
+		return q == nil // если оба nil, то вернём true
 	} else if q == nil {
 		return false
 	}
-	if p.Val != q.Val {
+	if p.Val != q.Val { // сравниваем значения одинаково расположенных ветвей
 		fmt.Printf("значения p = %d, q = %d - не совпали\n", p.Val, q.Val)
 		return false
 	}
 	fmt.Printf("значения p = %d, q = %d - совпали\n", p.Val, q.Val)
+	// проверяем симметрию наличия либо отсутствия ветвей-детей слева
 	if p.Left != nil && q.Left != nil || p.Left == nil && q.Left == nil {
 		fmt.Println("Наличие левых детей, одинаковое")
 	} else {
 		fmt.Println("Наличие левых детей, не одинаковое")
 		return false
 	}
+	// проверяем симметрию наличия либо отсутствия ветвей-детей слева
 	if p.Right != nil && q.Right != nil || p.Right == nil && q.Right == nil {
 		fmt.Println("Наличие правых детей, одинаковое")
 	} else {
 		fmt.Println("Наличие правых детей, не одинаковое")
 		return false
 	}
-
+	//проверяем значения детей-ветвей слева, если они существуют
 	if p.Left != nil {
 		fmt.Println("Проверяем левых детей, так как они существуют")
 		otvet := isSameTree(p.Left, q.Left)
@@ -33,6 +35,8 @@ func isSameTree(p *TreeNode, q *TreeNode) bool {
 			return false
 		}
 	}
+
+	//проверяем значения детей-ветвей справа, если они существуют
 	if p.Right != nil {
 		fmt.Println("Проверяем правых детей, так как они существуют")
 		otvet := isSameTree(p.Right, q.Right)
@@ -40,7 +44,7 @@ func isSameTree(p *TreeNode, q *TreeNode) bool {
 			return false
 		}
 	}
-
+	//все проверки пройдены, деревья одинаковые
 	return true
 }
 
